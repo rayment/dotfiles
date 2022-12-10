711e40d019b4082a54f25068f87a9b3a94c31c0f
Finn Rayment <finn@rayment.fr>
2022-12-10 16:43:56 +1100

    [xinitrc] Moved xinput commands into a new file.

---
+++ .config/x/xinput.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#
+# xinput.sh
+# XInput setup.
+#
+# Author       : Finn Rayment <finn@rayment.fr>
+# Date created : 08/10/2022
+#
+
+xinput set-prop "SynPS/2 Synaptics TouchPad" 286 2.5
+xinput set-prop "TPPS/2 Elan TrackPoint" "libinput Accel Speed" -0.7
+
